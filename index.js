6876bb86a4e14544f354073018a9a20e5beecdae
Sebastian Gonzalez <sebagonz91@gmail.com>
2015-09-17 16:30:54 -0300

    credentials deleted

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Instagram = require('instagram-node-lib');
 
-// var clientID = '69ab5b749fa34effb61d546762bf2c02',
-//     clientSecret = 'd32dba127b4a424a8c093ce08e7c282b';
-
 Instagram.set('client_id', process.env.INSTA_CLIENT_ID);
 Instagram.set('client_secret', process.env.INSTA_CLIENT_SECRET);
 Instagram.set('callback_url', process.env.INSTA_CALLBACK);
